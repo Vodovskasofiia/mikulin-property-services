14189f24ad66af85923979160c53579916bb7603
Sofiia <155563175+Vodovskasofiia@users.noreply.github.com>
2026-07-09 23:52:46 +0200

    Update translations.js

--- js/script.jsjs/translations.js
+++ js/script.jsjs/translations.js
@@ -1,1 +1,19 @@
+const translations = {
 
+    de:{
+
+        "hero-title":"Ihr zuverlässiger Partner für Immobilienbetreuung in Bern",
+
+        "contact-button":"Kontakt aufnehmen"
+
+    },
+
+    en:{
+
+        "hero-title":"Your reliable partner for property management in Bern",
+
+        "contact-button":"Contact us"
+
+    }
+
+};
