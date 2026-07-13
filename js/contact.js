6329f66b91970105737e28c5d9b95b08d653ee45
Sofiia <155563175+Vodovskasofiia@users.noreply.github.com>
2026-07-13 23:32:10 +0200

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,56 @@
+// ===============================
+// MPS CONTACT FORM
+// ===============================
 
+const checkin = document.getElementById("checkin");
+const checkout = document.getElementById("checkout");
+const nights = document.getElementById("nights");
+
+// сегодняшняя дата
+const today = new Date().toISOString().split("T")[0];
+
+// запрещаем выбирать прошедшие даты
+checkin.min = today;
+checkout.min = today;
+
+
+// когда меняется дата заезда
+checkin.addEventListener("change", () => {
+
+    // дата выезда не может быть раньше
+    checkout.min = checkin.value;
+
+    calculateNights();
+
+});
+
+
+// когда меняется дата выезда
+checkout.addEventListener("change", calculateNights);
+
+
+// подсчет ночей
+function calculateNights(){
+
+    if(checkin.value && checkout.value){
+
+        const start = new Date(checkin.value);
+        const end = new Date(checkout.value);
+
+        const diff = end - start;
+
+        const total = diff / (1000 * 60 * 60 * 24);
+
+        if(total > 0){
+
+            nights.value = total;
+
+        }else{
+
+            nights.value = "";
+
+        }
+
+    }
+
+}
